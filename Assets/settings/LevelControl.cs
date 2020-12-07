a9c09877e51fa93172b00919e48556fa001bb756
dimasbo <go.dimasbo@gmail.com>
2020-12-08 01:40:53 +0200

    settings

---
+++ Assets/settings/LevelControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Xml;
+
+public class LevelControl : MonoBehaviour {
+
+    [SerializeField]
+    private XmlDocument docSetting;
+   
+    void Start()
+    {
+        Debug.Log(docSetting);
+    }
+}
